2855e3bef12f6aad6b66b0c64906bc4ebed281b4
Jorge López <123602935+JorgeLpez@users.noreply.github.com>
2023-02-16 19:04:54 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-#include<locale.h>
-#include"Mamifero.h"
 #include"Felino.h"
 #include"GatoDomestico.h"
 
@@ -12,20 +9,28 @@ int main()
     int opc,opc2;
     string NewName;
     Felino EstrellaCirco("Carne",2020,"Zoo","Tigre","Atayde");
-   //Felino EstrellaCirco("Croquetas",2010,"Casa","Persa","Jorge");
-    GatoDomestico Minino("Croquetas",2010,"Casa","Persa","Jorge");
+    GatoDomestico Minino("Croquetas",2010,"Casa","Persa","Jose","Charly");
+    GatoDomestico Minino2("Croquetas",2010,"Casa","Persa","Jorge","Charly");
+
+    //uso del operador y constructor copia
+    Felino F=EstrellaCirco;
+    F.Imprimir();
+    //uso del operador y constructor move
+    GatoDomestico M(move(Minino2));
+    M.Imprimir();
+
 
     do{
-        system("cls");
-        cout<<"\n\t***MENÚ DE OPCIONES***"<<endl;
+        //system("cls");
+        cout<<"\n\t***MENÃš DE OPCIONES***"<<endl;
         cout<<"1. Dieta"<<endl;
-        cout<<"2. Año y lugar de nacimiento."<<endl;
-        cout<<"3. Cambiar nombre del dueño de Minino."<<endl;
+        cout<<"2. AÃ±o y lugar de nacimiento."<<endl;
+        cout<<"3. Cambiar nombre del dueÃ±o de Minino."<<endl;
         cout<<"4. Raza."<<endl;
-        cout<<"5. Cambiar nombre del circo en el que actúa EstrellaCirco."<<endl;
+        cout<<"5. Cambiar nombre del circo en el que actÃºa EstrellaCirco."<<endl;
         cout<<"6. Realizar operaciones con los objetos."<<endl;
         cout<<"7. Salir."<<endl;
-        cout<<"Ingresa la opción: ";
+        cout<<"Ingresa la opciÃ³n: ";
         cin>>opc;
 
         switch(opc){
@@ -38,18 +43,18 @@ int main()
 
             case 2: system("cls");
                     cout<<"\n\t***Estrella circo***";
-                    EstrellaCirco.Nacimiento();
+                    cout<<EstrellaCirco.getLugarNac()<<"\n"<<EstrellaCirco.getAnio()<<endl;
                     cout<<"\t***Minino***";
-                    Minino.Nacimiento();
+                    cout<<Minino.getLugarNac()<<"\n"<<Minino.getAnio()<<endl;
                     system("pause");
                     break;
 
             case 3: system("cls");
-                    cout<<"Nombre actual del dueño de minino: "<<Minino.getNombre();
+                    cout<<"Nombre actual del dueÃ±o de minino: "<<Minino.getDuenio();
                     cout<<"\n\nIngresa el nuevo nombre: ";
                     cin>>NewName;
                     Minino.CambiarNom(NewName);
-                    cout<<"\nNombre del dueño actualizado a '"<<Minino.getNombre()<<"' correctamente!"<<endl<<endl;
+                    cout<<"\nNombre del dueÃ±o actualizado a '"<<Minino.getDuenio()<<"' correctamente!"<<endl<<endl;
                     system("pause");
                     break;
 
@@ -69,22 +74,22 @@ int main()
                     break;
 
             case 6: system("cls");
-                    cout<<"Elije que operación deseas realizar: \n1. ==\n2. !=\n3. +\n4. <\n5. >\nIngresa la Opción: ";
+                    cout<<"Elije que operaciÃ³n deseas realizar: \n1. ==\n2. !=\n3. +\n4. <\n5. >\nIngresa la OpciÃ³n: ";
                     cin>>opc2;
                     if(opc2==1){
-                        cout<<"\nEstrella Circo == Minino "<<((EstrellaCirco==Minino)?"Si":"No")<<endl<<endl;
+                        cout<<"\nMinio2 == Minino "<<((Minino2==Minino)?"Si":"No")<<endl<<endl;
                     }else if(opc2==2){
-                        cout<<"\nEstrella Circo != Minino "<<((EstrellaCirco!=Minino)?"Si":"No")<<endl<<endl;
+                        cout<<"\nMinino2 != Minino "<<((Minino2!=Minino)?"Si":"No")<<endl<<endl;
                     }else if(opc2==3){
-                        cout<<"\nEstrella Circo + Minino"<<endl;
-                        Mamifero obj1=EstrellaCirco+Minino;
-                        obj1.Imprimir();
+                        cout<<"\nMinino2 + Minino"<<endl;
+                        GatoDomestico Minino3=Minino2+Minino;
+                        cout<<Minino3.getNombre()<<endl<<endl;
                     }else if(opc2==4){
-                        cout<<"\nEstrella Circo < Minino "<<((EstrellaCirco<Minino)?"Si":"No")<<endl<<endl;
+                        cout<<"\nMinino2 < Minino "<<((Minino2<Minino)?"Si":"No")<<endl<<endl;
                     }else if(opc2==5){
-                        cout<<"\nEstrella Circo > Minino "<<((EstrellaCirco>Minino)?"Si":"No")<<endl<<endl;
+                        cout<<"\nMinino2 > Minino "<<((Minino2>Minino)?"Si":"No")<<endl<<endl;
                     }else{
-                        cout<<"La opción no existe!"<<endl;
+                        cout<<"La opciÃ³n no existe!"<<endl;
                     }
                     system("pause");
                     break;
