5b1f408e9cf56f17054a3b90c0be19f3abad3f7c
Jorge López <123602935+JorgeLpez@users.noreply.github.com>
2023-02-16 19:06:26 -0600

    Update Mamifero.h

--- Mamifero.h
+++ Mamifero.h
@@ -1,1 +1,25 @@
-sadasd
+#ifndef MAMIFERO_H
+#define MAMIFERO_H
+#include<iostream>
+
+using namespace std;
+
+class Mamifero
+{
+    private:
+        int Anio,_length;
+        string Dieta,LugarNac,Raza,_data;
+
+    public:
+        Mamifero(string,int,string,string);
+        Mamifero(const Mamifero&);
+        Mamifero(Mamifero&&)noexcept;
+        string getDieta();
+        string getRaza();
+        string getLugarNac();
+        int getAnio();
+        int getEdad();
+        void Imprimir();
+};
+
+#endif // MAMIFERO_H
